781973c3d3727af1a9828946db72a3b95f0c58a5
Gulshan Anand <gulshan2052@gmail.com>
2023-03-18 13:34:25 +0530

    added gitignore

--- urldata.py
+++ urldata.py
@@ -7,10 +7,10 @@ class urldata:
     shortURL = None
     def __init__(self):
         self.db = sql.connect(
-            host = "10.0.0.4",
-            user = "linkadmin",
-            password = "cuttly",
-            database = "link"
+            host = "DB_HOST",
+            user = "DB_USER",
+            password = "DB_PASSWORD",
+            database = "DB_NAME"
         )
 
 
